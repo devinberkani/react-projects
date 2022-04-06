7b5f5bfbb6b96fc1ea58c4339b0a32efc7edf8d6
Devin Berkani <devinberkani@gmail.com>
2022-04-06 07:22:54 -0400

    complete projects 1-9

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,22 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  //sets const as data from data.js (5 people)
+  const [people, setPeople] = useState(data);
+
+  const clickHandler = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={clickHandler}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,44 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    let newData = data.slice(0, amount);
+    if (amount < 1) {
+      amount = 1;
+    } else if (amount > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((paragraph, index) => {
+          return <p key={index}>{paragraph}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
