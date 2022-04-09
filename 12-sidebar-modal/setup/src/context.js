c6098cf8d8a74617dfd5d0b32e7e59a2cf5b9b12
Devin Berkani <devinberkani@gmail.com>
2022-04-09 10:29:14 -0400

    complete sidebar/modal project

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,49 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [showModal, setshowModal] = useState(false);
+  const [showSidebar, setshowSidebar] = useState(false);
+
+  const openSidebar = () => {
+    setshowSidebar(true);
+  };
+
+  const closeSidebar = () => {
+    setshowSidebar(false);
+  };
+
+  const openModal = () => {
+    setshowModal(true);
+  };
+
+  const closeModal = () => {
+    setshowModal(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        showModal,
+        showSidebar,
+        openSidebar,
+        closeSidebar,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+//custom hook -- ********IMPORTANT********** -- MUST START WITH 'use'
+
+//must be invoked, you don't need to import useContext when doing it this way
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
