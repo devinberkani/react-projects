d94ef3fe275c1c684fe1031ea07db3121d8a10ca
Devin Berkani <devinberkani@gmail.com>
2022-04-13 07:20:11 -0400

    begin cocktails DB project

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from 'react';
+import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
-  return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+  const { setSearchTerm } = useGlobalContext();
 
-export default SearchForm
+  //search ref
+  const searchValue = React.useRef('');
+
+  //onChange function for input
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  //focus search field
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  //prevent refresh when user presses enter
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, nostrum
+        aliquam magni quasi dicta aspernatur incidunt expedita repudiandae
+        inventore! Consequatur deserunt repellat in reprehenderit blanditiis
+        vitae odit ipsa dolorem ad accusantium porro corporis, asperiores nihil?
+        Quas voluptatem ut quos magnam!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
